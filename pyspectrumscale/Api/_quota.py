53cb079da51e12a4f4287c8c36264c8ef67f6039
Aaron Hicks <aethylred@gmail.com>
2019-05-29 13:38:59 +1200

    Create a PreparedRequest object that will set a quota
    For IAM-168

--- pyspectrumscale/Api/_quota.py
+++ pyspectrumscale/Api/_quota.py
@@ -1,7 +1,9 @@
 """
 Methods for pyspectrumscale.Api that deal with quotas
 """
+import sys
 from typing import Union
+from ._utils import blocktoint, inodetoint, validgracestr
 
 
 def get_quota(
@@ -75,6 +77,7 @@ def quota(
             response = response[0]
 
     return response
+
 
 def quotas(
         self,
@@ -155,16 +158,138 @@ def quotas(
 ## These methods must make no changes if dryrun is true
 ##
 
+
 def preppost_quota(
-    self,
-    filesystem: type=str,
-    fileset: type=str,
-    blocksoftlimit: Union[None, str]=None,
-    blockhardlimit: Union[None, str]=None,
-    blockgraceperiod: Union[None, str]=None,
-    filessoftlimit: Union[None, str]=None,
-    fileshardlimit: Union[None, str]=None,
-    filesgraceperiod: Union[None, str]=None,
-    quotatype: Union[None, str]="FILESET"
+        self,
+        filesystem: type=str,
+        fileset: type=str,
+        blocksoftlimit: Union[None, str]=None,
+        blockhardlimit: Union[None, str]=None,
+        blockgraceperiod: Union[None, str]=None,
+        filessoftlimit: Union[None, str]=None,
+        fileshardlimit: Union[None, str]=None,
+        filesgraceperiod: Union[None, str]=None,
+        quotatype: Union[None, str]="FILESET"
 ):
-    print('do something')
+    """
+    @brief      Creates a requests.PreparedRequest object to create a quota.
+                This method makes NO CHECKS if the new quota is a good idea or not.
+                The processes that call this method should check if the new quota
+                would make the fileset/group/user unusable due to violating the
+                new quota (i.e. check if usage > new quota)
+                i.e. this methods assumes you know what you're doing
+
+    @param      self              The object
+    @param      filesystem        The filesystem
+    @param      fileset           The fileset
+    @param      blocksoftlimit    The blocksoftlimit
+    @param      blockhardlimit    The blockhardlimit
+    @param      blockgraceperiod  The blockgraceperiod
+    @param      filessoftlimit    The filessoftlimit
+    @param      fileshardlimit    The fileshardlimit
+    @param      filesgraceperiod  The filesgraceperiod
+    @param      quotatype         The quotatype
+
+    @return     { description_of_the_return_value }
+    """
+
+    # Convert parameters to int
+    blocksoftint = blocktoint(blocksoftlimit)
+    blockhardint = blocktoint(blockhardlimit)
+    filessoftint = inodetoint(filessoftlimit)
+    fileshardint = inodetoint(fileshardlimit)
+
+    validquota = True
+    reasons = []
+    prepresponse = None
+
+    # Is soft block quota < hard block quota
+    # and they both aren't 0
+    if blocksoftlimit is not None and blockhardlimit is not None:
+        if blocksoftint != 0 and blockhardint != 0:
+            if blocksoftint >= blockhardint:
+                reason = (
+                    "Soft block limit (%s) is not"
+                    " less than block hard limit (%s)" %
+                    (
+                        blocksoftlimit,
+                        blockhardlimit
+                    )
+                )
+                validquota = False
+                reasons.append(reason)
+
+    # Is soft block quota < hard block quota
+    # and they both aren't 0
+    if filessoftlimit is not None and fileshardlimit is not None:
+        if filessoftint != 0 and fileshardint != 0:
+            if filessoftint >= fileshardint:
+                reason = (
+                    "Soft inode limit (%s) is not"
+                    " less than inode hard limit (%s)" %
+                    (
+                        filessoftlimit,
+                        fileshardlimit
+                    )
+                )
+                validquota = False
+                reasons.append(reason)
+
+    if validquota:
+        if fileset is not None:
+            commandurl = (
+                "%sfilesystems/%s/quotas" % (
+                    self._base_url,
+                    filesystem
+                )
+            )
+        else:
+            commandurl = (
+                "%sfilesystems/%s/filesets/%s/quotas" % (
+                    self._base_url,
+                    filesystem,
+                    fileset
+                )
+            )
+
+        data = {
+            'operationType': 'setQuota',
+            'quotaType': quotatype,
+        }
+
+        if fileset is not None:
+            data['objectName'] = fileset
+
+        if blocksoftlimit is not None:
+            data['blockSoftLimit'] = blocksoftint
+
+        if blockhardlimit is not None:
+            data['blockHardLimit'] = blockhardint
+
+        if filessoftlimit is not None:
+            data['filesSoftLimit'] = filessoftint
+
+        if fileshardlimit is not None:
+            data['filesHardLimit'] = fileshardint
+
+        if filesgraceperiod is not None:
+            if validgracestr(filesgraceperiod):
+                data['filesGracePeriod'] = filesgraceperiod
+
+        if blockgraceperiod is not None:
+            if validgracestr(blockgraceperiod):
+                data['blockGracePeriod'] = blockgraceperiod
+
+        prepresponse = self._preppost(
+            commandurl=commandurl,
+            data=data
+        )
+
+    else:
+        for reason in reasons:
+            print(
+                ("ERROR: %s" % reason),
+                file=sys.stderr
+            )
+
+    return prepresponse
--- pyspectrumscale/Api/_utils.py
+++ pyspectrumscale/Api/_utils.py
@@ -5,6 +5,9 @@ import pathlib
 import urllib
 import requests
 import json
+import re
+import sys
+from quantities import kibi, mebi, gibi, tebi, pebi
 
 
 # This converts a file path into something safe
@@ -12,6 +15,9 @@ import json
 def truncsafepath(
     path: type=str
 ):
+    """
+    """
+
     pathtail = pathlib.Path(path).parts[2:]
     mountpath = str(pathlib.PurePath(*pathtail).as_posix())
     return urllib.parse.quote(mountpath, safe='')
@@ -36,3 +42,167 @@ def jsonprepreq(
 
     jsonprepreq['body'] = json.loads(preprequest.body)
     return jsonprepreq
+
+
+def validinodestr(
+        inodestr: str
+):
+    """
+    @brief      Checks that an Scale quota inode string is valid
+    """
+
+    inode_validator = re.compile("^\d+\.?\d*[KGM]?$")
+    return inode_validator.match(str(inodestr))
+
+
+def validblockstr(
+        blockstr: str
+):
+    """
+    @brief      Checks that an Scale quota block string is valid
+    """
+    block_validator = re.compile("^\d+\.?\d*[KGMTP]?$")
+    return block_validator.match(str(blockstr))
+
+
+def validgracestr(
+        gracestr: str
+):
+    """
+    @brief      Checks that an Scale grace time period string is valid
+    """
+    grace_validator = re.compile("^\d+\.?\d*(seconds|minutes|hours|days)$")
+    return grace_validator.match(str(gracestr))
+
+
+def blocktoint(
+        blockstr: str
+):
+    """
+    @brief      This function turns a block size string enting with K,M,G,T,P into a Kibi, Mibi, Gibi, Tibi, or Pibi int value respectivly.
+
+    @param      blockstr  The blockstr
+
+    @return     the int value of the blockstr
+    """
+
+    result = None
+
+    # is block string valid
+    if validblockstr(blockstr):
+        # is it just a digit
+        if isinstance(blockstr, int):
+            result = blockstr
+        elif blockstr.isdigit():
+            result = int(blockstr)
+        elif blockstr[-1] in ['K', 'M', 'G', 'T']:
+            # Process
+            if blockstr[-1] == 'K':
+                result = int(float(blockstr[:-1]) * kibi)
+            elif blockstr[-1] == 'M':
+                result = int(float(blockstr[:-1]) * mebi)
+            elif blockstr[-1] == 'G':
+                result = int(float(blockstr[:-1]) * gibi)
+            elif blockstr[-1] == 'T':
+                result = int(float(blockstr[:-1]) * tebi)
+            elif blockstr[-1] == 'P':
+                result = int(float(blockstr[:-1]) * pebi)
+    else:
+        print(
+            "ERROR: %s is not a valid block string for spectrumscale" %
+            blockstr,
+            file=sys.stderr
+        )
+
+    return result
+
+
+def inodetoint(
+        inodestr: str
+):
+    """
+    @brief      This function turns a inode size string enting with K,M,G into a Kibi, Mibi, or Gibi int value respectivly.
+
+    @param      blockstr  The blockstr
+
+    @return     the int value of the blockstr
+    """
+    result = None
+
+    # is block string valid
+    if validinodestr(inodestr):
+        # is it just a digit
+        if isinstance(inodestr, int):
+            result = inodestr
+        elif inodestr.isdigit():
+            result = int(inodestr)
+        elif inodestr[-1] in ['K', 'M', 'G']:
+            # Process
+            if inodestr[-1] == 'K':
+                result = int(float(inodestr[:-1]) * kibi)
+            elif inodestr[-1] == 'M':
+                result = int(float(inodestr[:-1]) * mebi)
+            elif inodestr[-1] == 'G':
+                result = int(float(inodestr[:-1]) * gibi)
+    else:
+        print(
+            "ERROR: %s is not a valid file/inode string for spectrumscale" %
+            inodestr,
+            file=sys.stderr
+        )
+
+    return result
+
+
+def blockcompare(
+        block1: str,
+        block2: str
+):
+    """
+    @brief      Compare two Scale block strings
+
+    @param      block1  The block 1
+    @param      block2  The block 2
+
+    @return     { description_of_the_return_value }
+    """
+    result = None
+    # Check if block1 and block 2 are valid
+    blkint1 = blocktoint(block1)
+    blkint2 = blocktoint(block2)
+
+    # if the blocks are invalid return none
+    if blkint1 is None or blkint2 is None:
+        return None
+
+    # Compare block1 and block2
+    result = blkint1 - blkint2
+
+    return int(result)
+
+
+def inodecompare(
+        inode1: str,
+        inode2: str
+):
+    """
+    @brief      Compares two Scale inode strings
+
+    @param      inode1  The inode 1
+    @param      inode2  The inode 2
+
+    @return     { description_of_the_return_value }
+    """
+    result = None
+    # Check if inode1 and inode 2 are valid
+    inodeint1 = inodetoint(inode1)
+    inodeint2 = inodetoint(inode2)
+
+    # if the blocks are invalid return none
+    if inodeint1 is None or inodeint2 is None:
+        return None
+
+    # Compare block1 and block2
+    result = inodeint1 - inodeint2
+
+    return int(result)
